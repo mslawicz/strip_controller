a07ebc979a281cb146198d3df1ffef01be51f690
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-10-09 21:16:08 +0200

    task creation; part 1, not finished

--- include/StripController.h
+++ include/StripController.h
@@ -1,3 +1,14 @@
 #pragma once
 
 void stripControllerInit(void);
+
+class StripController
+{
+    public:
+    StripController() = default;
+    static StripController& getInstance(void) { return stripController; }
+    void handler(void * pvParameter);
+
+    private:
+    static StripController stripController;
+};
--- src/StripController.cpp
+++ src/StripController.cpp
@@ -1,7 +1,24 @@
 #include "StripController.h"
+#include "cmsis_os2.h"
+#include "sl_cmsis_os2_common.h"
+#include <functional>
 
 #include "sl_pwm.h" //XXX test
 #include "sl_pwm_instances.h"   //XXX test
+
+#define SC_TASK_STACK_SIZE (1024)
+
+uint8_t striControllerStack[SC_TASK_STACK_SIZE];
+osThread_t stripControllerTaskControlBlock;
+constexpr osThreadAttr_t stripControllerTaskAttr = { .name = "StripControllerTask",
+                                         .attr_bits  = osThreadDetached,
+                                         .cb_mem     = &stripControllerTaskControlBlock,
+                                         .cb_size    = osThreadCbSize,
+                                         .stack_mem  = striControllerStack,
+                                         .stack_size = SC_TASK_STACK_SIZE,
+                                         .priority   = osPriorityBelowNormal };
+
+osThreadId_t sStripControllerTaskHandle;
 
 //initialize strip controller task
 void stripControllerInit(void)
@@ -9,4 +26,12 @@ void stripControllerInit(void)
     //XXX test of PWM
     sl_pwm_set_duty_cycle(&sl_pwm_WS2812_bit, 50);
     sl_pwm_start(&sl_pwm_WS2812_bit);
-}    
+
+    std::bind(&StripController::handler, &StripController::getInstance);
+    sStripControllerTaskHandle = osThreadNew((osThreadFunc_t*)std::bind(&StripController::handler, &StripController::getInstance()), nullptr, &stripControllerTaskAttr);
+}
+
+void StripController::handler(void *pvParameter)
+{
+
+}
