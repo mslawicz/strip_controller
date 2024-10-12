13215f351da7614b9876e272576bb9785f5b0e4c
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-10-12 19:10:40 +0200

    test of 8 leds OK

--- include/StripController.h
+++ include/StripController.h
@@ -2,8 +2,6 @@
 
 #include "cmsis_os2.h"
 #include "sl_cmsis_os2_common.h"
-#include <tuple>
-#include <array>
 
 #define SC_EVENT_ACTION_REQ         (1UL << 0)    //WS2812 data action request
 #define SC_EVENT_TRANSMIT_REQ       (1UL << 1)    //WS2812 buffer transmit request
@@ -16,7 +14,12 @@ struct StripControllerParams_t
     uint16_t numberOfDevices = 0;   //number of WS2812 devices in the strip
 };
 
-using RGB_t = std::tuple<uint8_t, uint8_t, uint8_t>;  
+struct RGB_t
+{
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+};
 class StripController
 {
     public:
@@ -25,7 +28,8 @@ class StripController
 
     private:
     StripControllerParams_t& params;
-    std::array<RGB_t, WS2812_NUMB_DEV> RGB_array;
+    void byteToPulses(uint8_t* pBuffer, uint8_t colorData);
+    void RGBToPulses(uint8_t* pBuffer, RGB_t RGB_data);
 };
 
 void stripControllerTaskInit(void);
--- src/StripController.cpp
+++ src/StripController.cpp
@@ -125,8 +125,46 @@ StripController::StripController(StripControllerParams_t& params) :
 
 uint32_t StripController::action(void)
 {
-    uint8_t testBuffer[] = {0x92, 0x49, 0xA4, 0x92, 0x49, 0x24, 0x92, 0x49, 0x24};  //green
-    std::memcpy(params.pBuffer, testBuffer, sizeof(testBuffer));
+    uint8_t fc = 30;
+    uint8_t hc = fc / 2;
+    uint8_t qc = fc / 3;
+    RGBToPulses(params.pBuffer, RGB_t{fc,0,0});     //XXX test red
+    RGBToPulses(params.pBuffer+9, RGB_t{hc,hc,0});     //XXX test yellow
+    RGBToPulses(params.pBuffer+18, RGB_t{0,fc,0});     //XXX test green
+    RGBToPulses(params.pBuffer+27, RGB_t{0,hc,hc});     //XXX test cyan
+    RGBToPulses(params.pBuffer+36, RGB_t{0,0,fc});     //XXX test blue
+    RGBToPulses(params.pBuffer+45, RGB_t{hc,0,hc});     //XXX test magenta
+    RGBToPulses(params.pBuffer+54, RGB_t{qc,qc,qc});     //XXX test gray
+    RGBToPulses(params.pBuffer+63, RGB_t{qc,qc,qc});     //XXX test gray    
+
     osEventFlagsSet(stripControllerFlags, SC_EVENT_TRANSMIT_REQ);
     return 20;
+}
+
+//codes one byte of color value into 3 bytes of WS2812 coded pulses at the address pBuffer
+void StripController::byteToPulses(uint8_t* pBuffer, uint8_t colorData)
+{
+    uint32_t pulseBuffer = 0;
+
+    for(uint8_t bit = 0; bit < 8; bit++)
+    {
+        pulseBuffer <<= 3;
+        //bit 0 is represented by the pulse codded with bits 100 (duty 33%)
+        //bit 1 is represented by the pulse codded with bits 110 (duty 66%)
+        pulseBuffer |= ((colorData << bit) & 0x80) == 0 ? 0x04 : 0x06;
+    }
+
+    //place bytes in the buffer in the big endian order
+    *pBuffer = (pulseBuffer >> 16) & 0xFF;
+    *(pBuffer + 1) = (pulseBuffer >> 8) & 0xFF;
+    *(pBuffer + 2) = pulseBuffer & 0xFF;
+}
+
+//codes 3 bytes of RGB color value into 9 bytes of WS2812 coded pulses at the address pBuffer
+void StripController::RGBToPulses(uint8_t* pBuffer, RGB_t RGB_data)
+{
+    //WS2812 requires G-R-B order of bytes
+    byteToPulses(pBuffer, RGB_data.G);
+    byteToPulses(pBuffer + 3, RGB_data.R);
+    byteToPulses(pBuffer + 6, RGB_data.B);
 }
