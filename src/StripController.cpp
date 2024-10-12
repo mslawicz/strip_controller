327c522fe7d6db0f851443ad1d9d6516310e519f
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-10-12 07:23:51 +0200

    WS2812_transmit function created; callback tested OK

--- src/StripController.cpp
+++ src/StripController.cpp
@@ -23,6 +23,7 @@ constexpr osThreadAttr_t stripControllerTaskAttr =
 uint8_t WS2812_buffer[WS2812_BUFFER_SIZE];
 
 void stripControllerHandler(void * pvParameter);
+void WS2812_transmit(void);
 void WS2812_transferComplete(SPIDRV_Handle_t handle, Ecode_t transferStatus, int itemsTransferred);
 
 #pragma GCC diagnostic push
@@ -50,21 +51,28 @@ void stripControllerHandler(void* pvParameter)
     while(1)
     {
         osDelay(10);
-        GPIO_PinOutToggle(test0_PORT, test0_PIN);
 
         //XXX SPI test
         WS2812_buffer[0] = 0x9B;
         WS2812_buffer[1] = 0x49;
         WS2812_buffer[2] = 0xB4;
         WS2812_buffer[3] = 0x00;
-        SPIDRV_MTransmit(sl_spidrv_WS2812_handle, WS2812_buffer, WS2812_BUFFER_SIZE, WS2812_transferComplete);
+        WS2812_transmit();
     }
 }
 
+void WS2812_transmit(void)
+{
+    GPIO_PinOutSet(test0_PORT, test0_PIN);
+    SPIDRV_MTransmit(sl_spidrv_WS2812_handle, WS2812_buffer, WS2812_BUFFER_SIZE, WS2812_transferComplete);
+    GPIO_PinOutToggle(test0_PORT, test0_PIN);
+    GPIO_PinOutToggle(test0_PORT, test0_PIN);
+}    
+
 void WS2812_transferComplete(SPIDRV_Handle_t handle, Ecode_t transferStatus, int itemsTransferred)
 {
-  if (transferStatus == ECODE_EMDRV_SPIDRV_OK)
-  {
-   // Success !
-  }
+    if (transferStatus == ECODE_EMDRV_SPIDRV_OK)
+    {
+        GPIO_PinOutClear(test0_PORT, test0_PIN);
+    }
 }
